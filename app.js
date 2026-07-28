00e4f27a90141d92ecac833035424474bb3cb74b
Mr-MichaelK <michael1kolanjian2@gmail.com>
2026-07-28 10:17:40 +0300

    Setup app and server.js

---
+++ app.js
@@ -0,0 +1,40 @@
+const path = require("node:path");
+const express = require("express");
+const session = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const prisma = require("./db/prisma.js");
+const passport = require("./config/passport");
+const { sessionSecret } = require("./config/environment");
+
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  session({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    },
+    secret: sessionSecret || "a secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+module.exports = app;
